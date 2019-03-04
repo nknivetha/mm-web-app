17a4d27a5e29a52de8789b46fcf111cbfccc68d6
Niv Karthikeyan <nkarthik@caltech.edu>
2019-03-03 16:09:59 -0800

    Add volunteer search page
    Create new search page for volunteers to use that only allows
    searching by volunteer name (need to change to be by volunteer
    email once that is stored in the db). Old search page will only
    be used by admins.

--- functions/index.js
+++ functions/index.js
@@ -16,11 +16,12 @@ admin.initializeApp();
 exports.logCaseAdded = functions.database.ref('')
     .onCreate((snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
-      const added = snapshot.toJSON();
+      const added = snapshot();
+      const addedObj = added.toJSON();
       // Get the case ID
-      const caseID = added.caseID;
+      const caseID = addedObj.caseID;
       // Get the collection that has been written to
-      const coll = snapshot.ref.root.child.key;
+      const coll = snapshot.ref.parent.key;
 
       // Add this new datasnapshot to the audit log
       // (reference path has a /1 at the end to signals this was
@@ -30,44 +31,42 @@ exports.logCaseAdded = functions.database.ref('')
 
 // Listens for existing cases being updated in any of the db's collections
 // and logs the old case info in the audit collection
-exports.logCaseUpdated = functions.database.ref('')
+exports.logCaseUpdated = functions.database.instance('miraclemessages-v2').ref('{coll}/{caseID}')
       .onUpdate((change, context) => {
 
       // Get the datasnapshot before the change was made
-      const old = change.before.toJSON();
-      // Get the case ID
-      const caseID = old.caseID;
+      const old = change.before;
+      const oldObj = old.toJSON();
+
       // Get the collection that was updated
-      const coll = old.ref.root.child.key;
+      console.log("Hiya it's me");
+      console.log("Let's try this: " + JSON.stringify(oldObj));
 
       // Add this updated datasnapshot to the audit log
       // Create the appropriate reference path for the audit log object
       // by seeing if this is a subsequent addition to an already existing
       // audit log object and, if so, adjusting the path accordingly
-      const addr = '/audits/'+coll+'/'+caseID+'/2';
-      if (database.ref(addr) != null) {
-        const num = 2;
-        while (database.ref(addr) != null) {
-          num += 1;
-          addr = '/audits/'+coll+'/'+caseID+'/'+String(num);
-        }
-      }
+      var addr = 'audits/'+context.params.coll+'/'+context.params.caseID+'/2';
+      console.log('Let us see: ' + addr);
 
+
+      console.log('No issues so far: '+addr);
       // Add this data snapshot to the audit log
-      return database.ref(addr).set(old);
+      return old.ref.root.child(addr).set(oldObj);
         });
 
 // Listens for existing cases being updated in any of the db's collections
 // and logs the old case info in the audit collection
-exports.logCaseUpdated = functions.database.ref('')
-      .onUpdate((change, context) => {
+exports.logCaseDeleted = functions.database.ref('')
+      .onDelete((change, context) => {
 
       // Get the datasnapshot before the change was made
-      const old = change.before.toJSON();
+      const old = change.before;
+      const oldObj = old.toJSON();
       // Get the case ID
-      const caseID = old.caseID;
+      const caseID = oldObj.caseID;
       // Get the collection that was updated
-      const coll = old.ref.root.child.key;
+      const coll = old.ref().parent.key;
 
       // Add this data snapshot to the audit log
       // (reference path has a /-1 at the end to signal this was
